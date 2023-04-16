125555f3743660965e8f937377cd5bcc7aab0a82
rohith <rohith3085@gmail.com>
2023-04-16 03:01:00 +0000

    Character Type Functions

---
+++ H107.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    //your code here
+    if(isupper(ch))
+    {
+        printf("%c",tolower(ch));
+    }
+    else
+    {
+        printf("%c",toupper(ch));
+    }
+
+    return 0;
+}
